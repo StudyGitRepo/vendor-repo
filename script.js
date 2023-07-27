b49b7cf76f31cf82928d786560ce7d3a135c2ff2
StudyGitRepo <140696832+StudyGitRepo@users.noreply.github.com>
2023-07-27 15:52:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 /* script.js */
+(function(){
+  document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('button').addEventListener('click', function(){
+      let text=document.getElementById('text').value;
+      alert('Длина текста: ' + text.length);
+    });
+  });
+})();
