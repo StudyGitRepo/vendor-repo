0158d8d2bce14f34706e34846bd543ecfc909b4d
StudyGitRepo <vmilyh@mail.ru>
2023-07-31 12:11:07 +0300

    some changed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/* script.js */
 (function(){
+  alert('YES');
   document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('button').addEventListener('click', function(){
       let text=document.getElementById('text').value;
